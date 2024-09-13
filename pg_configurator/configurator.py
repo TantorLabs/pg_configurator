6862a68e91efba195e48897287d97e977ca8be35
Evgeny Voropaev <evorop@gmail.com>
2024-09-13 16:11:39 +0800

    Added support of PG16 and defined an algorithm for the max_parallel_apply_workers_per_subscription parameter.

--- pg_configurator/configurator.py
+++ pg_configurator/configurator.py
@@ -556,7 +556,7 @@ class PGConfigurator:
             "--pg-version",
             help="PostgreSQL version, (default: %(default)s)",
             type=str,
-            choices=list(["9.6", "10", "11", "12", "13", "14", "15"]),
+            choices=list(["9.6", "10", "11", "12", "13", "14", "15", "16"]),
             default=mca["pg_version"]
         )
         parser.add_argument(
